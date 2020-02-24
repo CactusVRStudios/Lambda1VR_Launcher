e4785c28afa5953ace45b1ac28d1e9ea242847dc
deadpressed <lauerbernd42@gmail.com>
2020-02-24 20:39:41 +0100

    Initial Commit
    Working Beta

---
+++ Assets/Start_HL1_Opforce.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Start_HL1_Opforce : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.gameObject.GetComponentInChildren<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        StreamWriter writer = new StreamWriter("/sdcard/xash/commandline.txt", false);
+        writer.WriteLine("xash3d -log -dev 3 -log -game gearbox");
+        writer.Close();
+
+
+        bool fail = false;
+        string bundleId = "com.drbeef.lambda1vr"; // your target bundle id
+        AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ca = up.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject packageManager = ca.Call<AndroidJavaObject>("getPackageManager");
+
+        AndroidJavaObject launchIntent = null;
+        try
+        {
+            launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", bundleId);
+        }
+        catch (System.Exception)
+        {
+            fail = true;
+        }
+
+        if (fail)
+        { //open app in store
+            Application.OpenURL("https://sidequest.com");
+        }
+        else //open the app
+            ca.Call("startActivity", launchIntent);
+
+        up.Dispose();
+        ca.Dispose();
+        packageManager.Dispose();
+        launchIntent.Dispose();
+
+    }
+}
---
+++ Assets/scrip/collionssound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collionssound : MonoBehaviour
+{
+
+    AudioSource audioData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        audioData.Play(0);
+    }
+
+
+}
